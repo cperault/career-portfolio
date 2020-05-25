52ea192949029e65799dcdac9632d64f6d662358
Christopher Perault <46505965+cperault@users.noreply.github.com>
2020-05-25 14:41:50 -0500

    Edited landing page text.

--- src/Components/Landing/LandingBody.js
+++ src/Components/Landing/LandingBody.js
@@ -11,12 +11,12 @@ const LandingBody = () => {
                     </div>
                     <div>
                         <p>I&apos;m a developer living in the heart of the Midwest. My favorite thing to do alone is watch horror in the dark. One day, I <span class="emphasis">will</span> have a corgi.</p>
-                        <p>I&apos;m currently in search of a new opportunity! What&apos;s my idea of a great work environment? One where there is:</p>
+                        <p>I&apos;m currently in search of a new opportunity! My ideal work environment is one where there is:</p>
                         <ul>
-                            <li><span class="values">Acceptance</span>; I believe in accepting and respecting all people, regardless of that of which personal identity is composed. I hope that you do, too.</li>
-                            <li><span class="values">Communication</span>; a goal cannot be met efficiently with a lack of communication between team members. I give and expect great effort on this.</li>
-                            <li><span class="values">Understanding</span>; I believe that it is important to make an effort to understand one another. Language&mdash;in any form&mdash;is beautiful when treated with utmost care; slow down, rephrase your thought, give clearer expectations. One thing that resonates with me is when someone talks to be understood&mdash;not just heard. We must also listen to understand&mdash;not to hear.</li>
-                            <li><span class="values">Transparency</span>; always be honest with your intentions. An average business will often make a decision based on profit. A great business understands that business is nothing without the customers or its employees. A fantastic business can do it all. </li>
+                            <li><span class="values">Acceptance</span>; I believe in accepting and respecting one another, including our differences.</li>
+                            <li><span class="values">Communication</span>; a goal cannot be met efficiently with a lack of communication between team members.</li>
+                            <li><span class="values">Understanding</span>; I believe that it is important to make an effort to understand one another. Language&mdash;in any form&mdash;is beautiful when treated with utmost care; slow down, rephrase your thought, give clearer expectations.</li>
+                            <li><span class="values">Transparency</span>; always be honest with your intentions. An average business will often make a decision based on profit. A great business understands that business is nothing without the customers or its employees. A fantastic business can do all three. </li>
                         </ul>
                     </div>
                 </div>
